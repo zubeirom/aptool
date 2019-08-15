5ecd4ff8400563da3cf5726714b20e041bf907c6
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-15 20:49:50 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// cors
+app.use(cors());
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
