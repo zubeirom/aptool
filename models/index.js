329cf21d23ecd3effea9a26bf20574ddfd3c36fd
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-16 01:58:39 +0200

    fix error

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname }/../config/config.json`)[env];
+// eslint-disable-next-line import/no-dynamic-require
+const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
 let sequelize;
