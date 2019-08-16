706c342025a7419f70589188480fd530175cd430
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-17 01:36:35 +0200

    Small setup of token route

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,15 @@
-// TODO: implement token route
+require('dotenv').config();
+const express = require('express');
+const utils = require('../utils/index');
+const asyncHandler = require('express-async-handler');
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('./config/private.key', 'utf8');
+
+const router = express.Router();
+
+router.post('/api/token', asyncHandler(async (req, res, next) => {
+    const { grant_type, password, username } = req.body;
+}))
+
+module.exports = router
