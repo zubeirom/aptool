41cbe3465a9a4efcde77904c12711664bcdf1990
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-17 02:01:24 +0200

    Add get route for account

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const utils = require('../utils/index');
 const asyncHandler = require('express-async-handler');
 const fs = require('fs');
+const accountController = require('../controllers/account');
 
 const privateKey = fs.readFileSync('./config/private.key', 'utf8');
 
@@ -11,5 +12,9 @@ const router = express.Router();
 router.post('/api/token', asyncHandler(async (req, res, next) => {
     const { grant_type, password, username } = req.body;
 }))
+
+/**ACCOUNT ROUTE*/
+router.get('/api/accounts', asyncHandler(accountController.get(req, res, next)))
+
 
 module.exports = router
