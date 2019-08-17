27756b886af308718c310d522f2c05998fed844a
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-17 21:30:46 +0200

    singularize position

--- migrations/20190817192355-create-position.js
+++ migrations/20190817192355-create-position.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('positions', {
+    return queryInterface.createTable('position', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -22,6 +22,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('positions');
+    return queryInterface.dropTable('position');
   }
 };
