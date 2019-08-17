77158be45f8b62f85eae1c5fe028ef930dfcd841
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-17 02:01:45 +0200

    Add get function for account

--- controllers/account.js
+++ controllers/account.js
@@ -1,4 +1,21 @@
-// const { Account } = require('../models');
-// const { Application } = require('../models');
+const { Account } = require('../models');
+const utils = require('../utils/index');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
-module.exports = {};
+const privateKey = fs.readFileSync('./config/private.key', 'utf8');
+
+module.exports = {
+    async get(req, res, next) {
+        try {
+            const access_token = utils.getAccessToken(req);
+            const payload = await jwt.verify(access_token, privateKey);
+            const { id } = payload;
+            const account = Account.findByPk(id);
+            res.status(200).send(account);
+            next();
+        } catch (error) {
+            next(error);
+        }
+    }
+};
