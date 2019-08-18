79f859bd503ea36c84f4ccf1cdd83c7a88ce303d
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-19 00:48:44 +0200

    Singularize event table name

--- migrations/20190818222847-create-event.js
+++ migrations/20190818222847-create-event.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.createTable('events', {
+    up: (queryInterface, Sequelize) => queryInterface.createTable('event', {
         id: {
             allowNull: false,
             autoIncrement: true,
@@ -36,5 +36,5 @@ module.exports = {
             type: Sequelize.DATE,
         },
     }),
-    down: (queryInterface /* Sequelize */) => queryInterface.dropTable('events'),
+    down: (queryInterface /* Sequelize */) => queryInterface.dropTable('event'),
 };
