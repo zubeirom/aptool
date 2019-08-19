58b402bcb28d3573a16095f61a2865914675dc9b
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-19 03:07:56 +0200

    Linting

--- models/position.js
+++ models/position.js
@@ -1,10 +1,11 @@
-'use strict';
+/* eslint-disable func-names */
+
 module.exports = (sequelize, DataTypes) => {
-  const position = sequelize.define('position', {
-    name: DataTypes.STRING
-  }, {});
-  position.associate = function(models) {
+    const position = sequelize.define('position', {
+        name: DataTypes.STRING,
+    }, {});
+    position.associate = function (/* models */) {
     // associations can be defined here
-  };
-  return position;
+    };
+    return position;
 };
