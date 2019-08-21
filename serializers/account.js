6b22d26df0df3dcc77f23005c676bde981cf38d5
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-21 23:17:48 +0200

    Remove password from serializer

--- serializers/account.js
+++ serializers/account.js
@@ -3,7 +3,7 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 module.exports = {
     serialize(data) {
         const AccountSerializer = new JSONAPISerializer('accounts', {
-            attributes: ['username', 'firstname', 'lastname', 'email', 'statuses', 'jobKeywords', 'password'],
+            attributes: ['username', 'firstname', 'lastname', 'email', 'statuses', 'jobKeywords'],
         });
 
         return AccountSerializer.serialize(data);
