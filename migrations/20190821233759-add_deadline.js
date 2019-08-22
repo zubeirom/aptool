df5c2315dd56c311ed9f14b5736a8e680f2c2f27
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-22 21:47:29 +0200

    Change deadline from timestamp to dateonly

--- migrations/20190821233759-add_deadline.js
+++ migrations/20190821233759-add_deadline.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.addColumn('application', 'deadline', Sequelize.DATE),
+    up: (queryInterface, Sequelize) => queryInterface.addColumn('application', 'deadline', Sequelize.DATEONLY),
 
     down: (queryInterface /* Sequelize */) => queryInterface.removeColumn('application', 'deadline'),
 };
--- serializers/application.js
+++ serializers/application.js
@@ -3,7 +3,7 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 module.exports = {
     serialize(data) {
         const ApplicationSerializer = new JSONAPISerializer('application', {
-            attributes: ['account_id', 'company', 'created', 'status', 'submissionType', 'source', 'recruiters', 'phases', 'position', 'account'],
+            attributes: ['account_id', 'company', 'created', 'status', 'source', 'recruiter', 'phases', 'occupation', 'deadline', 'account', 'note'],
             account: {
                 ref: 'id',
                 attributes: ['username', 'firstname', 'lastname', 'email', 'statuses', 'jobKeywords', 'password'],
