b1756a9ed4210390375b65242d40295bef48ea2f
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-31 04:09:16 +0200

    Add posting _url to serializer

--- serializers/application.js
+++ serializers/application.js
@@ -3,7 +3,7 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 module.exports = {
     serialize(data) {
         const ApplicationSerializer = new JSONAPISerializer('application', {
-            attributes: ['account_id', 'company', 'created', 'status', 'source', 'recruiter', 'phases', 'occupation', 'deadline', 'account', 'note', 'events'],
+            attributes: ['account_id', 'company', 'created', 'status', 'source', 'recruiter', 'phases', 'occupation', 'deadline', 'account', 'note', 'events', 'posting_url'],
             account: {
                 ref: 'id',
                 attributes: ['username', 'firstname', 'lastname', 'email', 'statuses', 'jobKeywords', 'password'],
