ae4e0d994f6690a648ddf53e7b9125f78bbd5ef2
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-09-02 18:22:40 +0200

    Add sentry

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: 'https://919db697e951412f962235e5f02c43c3@sentry.io/1549178' });
 
 const app = express();
+
+// Sentry
+app.use(Sentry.Handlers.requestHandler());
 
 // Bodyparser
 app.use(express.urlencoded({ extended: false }));
@@ -11,6 +17,11 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // cors
 app.use(cors());
+
+
+// Sentry error middleware
+app.use(Sentry.Handlers.errorHandler());
+
 
 // Express-jwt middleware
 app.use((err, req, res, next) => {
@@ -24,5 +35,4 @@ app.use((err, req, res, next) => {
 app.use('/', require('./routes/index'));
 
 const { PORT } = process.env;
-
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
